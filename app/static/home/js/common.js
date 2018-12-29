af0ecc1ccf6d52a22b0881299e8bb106b1eb659a
kerrygao <1509699669@qq.com>
2018-12-29 16:19:16 +0800

    aa

--- app/static/home/js/common.js
+++ app/static/home/js/common.js
@@ -1,50 +1,27 @@
-$(function(){
+layui.define(['layer', 'element'], function(exports){
+
+  var $ = layui.jquery
+  ,layer = layui.layer
+  ,element = layui.element
+  ,device = layui.device();
+
+  //阻止IE7以下访问
+  if(device.ie && device.ie < 10){
+    layer.alert('如果您非得使用 IE 浏览器,那么请使用 IE10+');
+  }
+
+  //搜索区域固定在上
+  var topbar = $('.search-nav'),fixTop = function(){
+    var top = $(window).scrollTop();
+    if(top > 60){
+      topbar.addClass('scroll');
+    } else {
+      topbar.removeClass('scroll');
+    }
+  };
+
+  fixTop(), $(window).on('scroll', fixTop);
 
 
-    $('.checkbox').click(function () {
-       if($(this).hasClass('active')){
-           $(this).removeClass('active');
-           $(this).find('input').val(0);
-       }else{
-           $(this).addClass('active');
-           $(this).find('input').val(1);
-       }
-    });
+});
 
-    var tmp = 1
-
-    $('#search_icon').click(function(){
-        if(tmp){
-            $('header .nav').css('display',"none");
-            $('.search_from').css('display',"block");
-            $(this).css('display',"none");
-            tmp = 0
-        }else{
-            $('header .nav').css('display',"block");
-            $('.search_from').css('display',"none");
-            $(this).css('display',"block");
-            tmp = 1
-        }
-
-    })
-
-    $('.close_search_icon').click(function(){
-            $('header .nav').css('display',"block");
-            $('.search_from').css('display',"none");
-            $('#search_icon').css('display',"block");
-            tmp = 1;
-    })
-
-    $("input[name=search]").keydown(function(e){
-
-        if (e.keyCode == "13") {
-            if ($(this).val() !== "") {
-                window.location.href = "?keywords="+ $(this).val();
-                // alert($(this).val())
-            };
-        };
-
-    })
-
-
-})
